ca2f012c931fc5197cb3641954773873287719e0
Abril Mejia <abrilmejia812@gmail.com>
2026-03-30 00:43:50 -0600

    se agrega funcion toogle para mostrar el simulador interactivo, se cambia el nombre de la funcion mostrarSimulador a mostrarSimuladorInteractivo, se cambia el id del div del simulador a simulador-interactivo, se agregan clases a los botones y se cambia el orden de los botones en index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,19 @@ function mostrarSimuladorInteractivo() {
 function mostrarSimuladorGUI() {
   document.getElementById("simulador-GUI").style.display = "block";
 }
+
+function toggleSimulador(id) {
+  let sim1 = document.getElementById("simulador-interactivo");
+  let sim2 = document.getElementById("simulador-GUI");
+
+  let actual = document.getElementById(id);
+
+  // Ocultar ambos primero
+  sim1.style.display = "none";
+  sim2.style.display = "none";
+
+  // Si el que clickeaste estaba oculto → mostrarlo
+  if (actual.style.display === "none") {
+    actual.style.display = "block";
+  }
+}
