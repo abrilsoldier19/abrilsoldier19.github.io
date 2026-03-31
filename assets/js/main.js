7bb84be87026e3831e35c8cc273148a33603c834
Abril Mejia <abrilmejia812@gmail.com>
2026-03-30 23:47:14 -0600

    se agregaron cambios a la vista de la opción h) del ejercicio 1, se creó un nuevo archivo CSS para esta opción y se realizaron cambios en el archivo main.js para agregar la funcionalidad de mostrar la fórmula al hacer clic en el botón correspondiente. Además, se realizaron ajustes en el diseño y estilo de la página para mejorar su apariencia.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,44 @@ function toggleSimulador(id) {
     actual.style.display = "block";
   }
 }
+
+
+// Función para realizar la operación en opcion h) Accionamiento por cremallera y por tornillo sin fin
+function operacion() {
+          var z = parseFloat(document.getElementById('z').value);
+          var m = parseFloat(document.getElementById('m').value);
+    
+          var s = z * m * Math.PI;
+
+          document.getElementById('resultado_s').innerHTML = "" + s;
+        }
+
+
+        function mostrarImagen() {
+        var imageContainer = document.getElementById('imageContainer');
+        var image = document.createElement('img');
+        image.src = '/static/cremallera_opcion_h).png'; // Reemplaza con la ruta de tu imagen
+        image.style.width = '350px'; // Set the desired width
+        image.style.height = '250px';
+        
+        // Agregar estilo para el efecto de luz
+        image.style.borderRadius = '10px';
+        image.style.boxShadow = '0 0 20px rgba(255, 255, 255, 0.7)'; // Cambia el valor según tus preferencias
+
+        imageContainer.innerHTML = ''; // Limpiar el contenedor de imágenes
+        imageContainer.style.display = 'flex';
+        imageContainer.style.justifyContent = 'center';
+        imageContainer.style.alignItems = 'center';
+        imageContainer.appendChild(image); // Agregar la imagen al contenedor
+    }
+  $(function () {
+    $('[data-toggle="popover"]').popover({
+      template: '<div class="popover custom-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    })
+      .on('shown.bs.popover', function () {
+        MathJax.Hub.Queue(['Typeset', MathJax.Hub, $(this).next('.popover').find('.popover-body')[0]]);
+      });
+  });
+    $(function() {
+        $('[data-toggle="popover"]').popover();
+    });
