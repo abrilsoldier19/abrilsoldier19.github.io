81a904e86ceb83f2b1b4eddb23acc90f03c0903a
Abril Mejia <abrilmejia812@gmail.com>
2026-04-27 17:13:24 -0600

    se corrige

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,20 +1,39 @@
 function loadHTML(id, file) {
+  const element = document.getElementById(id);
+
+  if (!element) return;
+
   fetch(file)
-    .then(res => res.text())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`No se pudo cargar: ${file}`);
+      }
+      return response.text();
+    })
     .then(data => {
-      document.getElementById(id).innerHTML = data;
+      element.innerHTML = data;
+    })
+    .catch(error => {
+      console.error(error);
     });
 }
 
-// detectar nivel de carpeta
-let basePath = "";
+function getBasePath() {
+  const path = window.location.pathname;
 
-if (window.location.pathname.includes("/vistas/ejercicios")) {
-  basePath = "../../";
-} else if (window.location.pathname.includes("/vistas")) {
-  basePath = "../";
+  // index.html en raíz
+  if (!path.includes("/vistas/")) {
+    return "";
+  }
+
+  // cuenta cuántas carpetas hay después de /vistas/
+  const afterVistas = path.split("/vistas/")[1];
+  const folders = afterVistas.split("/").slice(0, -1);
+
+  return "../".repeat(folders.length + 1);
 }
 
-// cargar archivos
-loadHTML("header", basePath + "layout/header.html");
-loadHTML("footer", basePath + "layout/footer.html");
+const basePath = getBasePath();
+
+loadHTML("header", basePath + "vistas/layout/header.html");
+loadHTML("footer", basePath + "vistas/layout/footer.html");
