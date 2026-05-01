fa8458361cb0cbbc306fa307cceb3056756e16e3
Abril Mejia <abrilmejia812@gmail.com>
2026-04-30 21:50:21 -0600

    se agrega el mismo estilo de ia y iia en iiia, se agrega la funcion para calcular la longitud de la correa en iiia y se agrega la funcion para mostrar la imagen de iiia, se corrige el texto del simulador interactivo de iia

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,6 +100,47 @@ function operacion_ia() {
         $('[data-toggle="popover"]').popover();
     });
 
+// Función para realizar la operación en opcion iiia)Simulacion Simulacion del calculo de la longitud de la correa que se necesita en mm
+    function operacion_iiia() {
+          var label_num_longitud1 = parseFloat(document.getElementById('label_num_longitud1').value);
+          var label_num_longitud2 = parseFloat(document.getElementById('label_num_longitud2').value);
+          var label_num_longitud3 = parseFloat(document.getElementById('label_num_longitud3').value);
+    
+          var L = Math.PI*(label_num_longitud1 + label_num_longitud2) + 2 * Math.sqrt(Math.pow(label_num_longitud3,2) + Math.pow(label_num_longitud1 - label_num_longitud2, 2));
+    
+          document.getElementById('resultado').innerHTML = "" + L;
+        }
+
+        function mostrarImagen_iiia() {
+            var imageContainer = document.getElementById('imageContainer');
+            var image = document.createElement('img');
+            image.src = 'static/longitud_correa.png'; // Reemplaza con la ruta de tu imagen
+            image.style.width = '350px'; // Set the desired width
+            image.style.height = '250px';
+        
+            // Agregar estilo para el efecto de luz
+            image.style.borderRadius = '10px';
+            image.style.boxShadow = '0 0 20px rgba(255, 255, 255, 0.7)'; // Cambia el valor según tus preferencias
+
+            imageContainer.innerHTML = ''; // Limpiar el contenedor de imágenes
+            imageContainer.style.display = 'flex';
+            imageContainer.style.justifyContent = 'center';
+            imageContainer.style.alignItems = 'center';
+            imageContainer.appendChild(image); // Agregar la imagen al contenedor
+    }
+
+  (function () {
+    $('[data-toggle="popover"]').popover({
+      template: '<div class="popover custom-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    })
+      .on('shown.bs.popover', function () {
+        MathJax.Hub.Queue(['Typeset', MathJax.Hub, $(this).next('.popover').find('.popover-body')[0]]);
+      });
+  });
+    (function() {
+        $('[data-toggle="popover"]').popover();
+    });
+
 // Función para realizar la operación en opcion h) Accionamiento por cremallera y por tornillo sin fin
 function operacion() {
           var z = parseFloat(document.getElementById('z').value);
